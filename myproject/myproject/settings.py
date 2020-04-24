e97067da5ec87cfca990b50705dd1e3aa5280e0e
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-04-24 18:14:47 +0500

    +ckeditor

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -131,8 +131,8 @@ CKEDITOR_UPLOAD_PATH = 'uploads/'
 
 CKEDITOR_CONFIGS = {
     'default': {
-        'skin': 'moono',
-        # 'skin': 'office2013',
+        # 'skin': 'moono',
+        'skin': 'office2013',
         'toolbar_Basic': [
             ['Source', '-', 'Bold', 'Italic']
         ],
