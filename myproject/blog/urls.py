02a8fe72ccfb264530df789e759fc5426881b1e6
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-04-24 19:29:56 +0500

    add pagination

--- myproject/blog/urls.py
+++ myproject/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home_view, PostListView
+from .views import home_view, PostListView, PostDetailView
 
 app_name = 'blog'
 urlpatterns = [
     path('', home_view, name='home_view'),
     path('posts/', PostListView.as_view(), name='post_list_view'),
+    path('posts/<int:id>/', PostDetailView.as_view(), name='post_detail_view'),
 ]
--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -11,3 +11,15 @@ class PostListView(ListView):
     template_name = 'post/list_view.html'
     queryset = Post.published.all()
 
+    model = Post
+    context_object_name = 'pages'
+    paginate_by = 2
+
+
+
+class PostDetailView(DetailView):
+    template_name = 'post/detail_view.html'
+
+    def get_object(self):
+        id_ = self.kwargs.get('id')
+        return get_object_or_404(Post, id=id_)
