258d7482e4c0635a79959f681ec7ed0b5463b2e5
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-04-24 14:32:04 +0500

    ckeditor

--- myproject/blog/admin.py
+++ myproject/blog/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 from .models import Post
+from django import forms
+
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class PostAdminForm(forms.ModelForm):
+    body = forms.CharField(label=('Описание'), widget=CKEditorUploadingWidget())
+    title=forms.CharField(label='Заголовок')
+
+    class Meta:
+        model = Post
+        fields = '__all__'
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'status')
-    list_filter = ('status','title',)
+    list_filter = ('status', 'title',)
     search_fields = ('title', 'body')
     ordering = ('status',)
+    form = PostAdminForm
